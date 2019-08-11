92bba1d175000d2f6d5783193dd1d0f4ec5c5917
Ben <sr365@duke.edu>
2019-08-11 17:52:11 -0400

    waitNrun update!! Now it can send you email with Job, PID, Start Time and the directory information along with your note!

--- Tandem/waitandrun.sh
+++ Tandem/waitandrun.sh
@@ -4,14 +4,26 @@ do
     echo "Process: $PID is still running" 
         sleep 10m
 done
+TIME=`date`
+PWD=`pwd`
+COMMAND=train.py
+SPACE='        '
 
-nohup python train.py 1>running.log 2>running.err 
+nohup python $COMMAND 1>running.log 2>running.err 
+echo $! > pidfile.txt
+
 
 {
 	echo To: rensimiao.ben@gmail.com
 	echo From: Cerus Machine
 	echo Subject: Your Job has finished!
-	echo "Hello your mighty Machine Learning researcher, your job has been finished and again, you saved so many fairies!!!"
+	echo -e "Dear mighty Machine Learning researcher Ben, \n \n"
+	echo -e "    Your job has been finished and again, you saved so many fairies!!!\n \n"
+	echo -e "Details of your job:\n 
+	Job:  $COMMAND \n  
+	PID:   `cat pidfile.txt` \n 
+	StartTime:   $TIME \n 
+	PWD:  $PWD\n"
 } | ssmtp rensimiao.ben@gmail.com
 echo "Process $PID has finished"
 
