Bug fixed for Backprop model training issue. Largely reusing the Tandem strusture code which is fully testd

Backprop/Backprop_network_maker.py
@@ -58,7 +58,7 @@ class BackPropCnnNetwork(object):
         if self.labels==[]:
             print('labels list is empty')
         else:
-            self.loss = self.make_loss()
+            self.loss, self.mse_loss, self.reg_loss, self.bdy_loss = self.make_loss()
             self.optm = self.make_optimizer()
             self.backprop_optm = self.make_backprop_optimizer()
   
@@ -96,10 +96,11 @@ class BackPropCnnNetwork(object):
         :return: mean cross entropy loss of the batch
         """
         with tf.variable_scope('loss'):
-            loss = tf.losses.mean_squared_error(self.labels, self.logits)
-            loss += tf.losses.get_regularization_loss()
-            loss += self.Boundary_loss
-            return loss
+            mse_loss = tf.losses.mean_squared_error(self.labels, self.logits) #reconstruction loss
+            reg_loss = tf.losses.get_regularization_loss()      #regularizaiton loss
+            bdy_loss = self.Boundary_loss                       #boundary loss
+            total_loss = mse_loss + reg_loss + bdy_loss         #Total loss
+            return total_loss, mse_loss, reg_loss, bdy_loss
             
     def make_optimizer(self):
         """
@@ -166,11 +167,48 @@ class BackPropCnnNetwork(object):
             
             ##Train the forward model
             for i in range(int(step_num)):
-                sess.run(self.optm)
+                sess.run([train_init_op, assign_true_op])
+                [feature, optm_out] = sess.run([self.features ,self.optm])
+                if (i % 500 == 0):
+                    print("Feature now is:", feature[0,:])
                 for hook in forward_hooks:
                     hook.run(sess, writer=summary_writer)
+                if forward_hooks[-1].stop:
+                    break
             
             self.save(sess)
+    
+    def evaluate_one(self, target_spectra, back_prop_epoch, sess, verb_step, stop_thres, point_index):
+        """
+        The function that evaluate one single given target spectra and return the results
+        :param target_spectra: The target spectra to back prop towards. Should be only 1 row
+        :param back_prop_epoch: #epochs to do the gradient descend
+        :param sess: The current session to do the back prop
+        """
+
+        #Set up target output
+        print("shape before repeat",np.shape(target_spectra.values))
+        target_spectra_repeat = np.repeat(np.reshape(target_spectra.values,(1,-1)), self.batch_size, axis = 0)
+        print("Size of the target spectra repeat", np.shape(target_spectra_repeat))
+        #target_spectra_dataset = tf.data.Dataset.from_tensor_slices(target_spectra_repeat)
+        #target_spectra_dataset = target_spectra_dataset.repeat()
+        for i in range(back_prop_epoch):
+            loss_back_prop, optm_out, inferred_spectra = sess.run([self.loss, self.backprop_optm, self.logits], 
+                                                                feed_dict={self.labels: target_spectra_repeat})  
+            if (i % verb_step == 0):
+                print("Loss at inference step{} : {}".format(i,loss_back_prop))
+                if (loss_back_prop < stop_thres):
+                    print("Loss is lower than the threshold{}, inference stop".format(stop_thres))
+
+        #Then it is time to get the best performing one
+        Xpred, Ypred = sess.run([self.forward_in, self.logits])
+        loss_list = np.linalg.norm(Ypred - target_spectra_repeat, axis = 1)
+        best_estimate_index = np.argmin(loss_list)
+        print('Best error for point {} is having absolute error of {}'.formate(point_index, loss_list[best_estimate_index]))
+        Xpred_best = Xpred[best_estimate_index,:]
+        Ypred_best = Ypred[best_estimate_index,:]
+        return Xpred_best, Ypred_best
+        
 
     def evaluate(self, valid_init_op, train_init_op, ckpt_dir,verb_step = 500, 
                 back_prop_ephoch = 10000, stop_thres = 1e-3,
@@ -226,44 +264,10 @@ class BackPropCnnNetwork(object):
                 assign_var_op = self.geometry_variable.assign(RN) #Assign the variable
                 sess.run([assign_var_op, train_init_op])
                 for i in range(h):
-                    Xpred, Ypred = evaluate_one(Ytruth.iloc[i,:], back_prop_ephoch, sess, verb_step, stop_thres, i)
+                    Xpred, Ypred = self.evaluate_one(Ytruth.iloc[i,:], back_prop_ephoch, sess, verb_step, stop_thres, i)
                     np.savetxt(f1, Xpred, fmt='%.3f')
                     np.savetxt(f3, Ypred, fmt='%.3f')
     
-    def evaluate_one(self, target_spectra, back_prop_epoch, sess, verb_step, stop_thres, point_index):
-        """
-        The function that evaluate one single given target spectra and return the results
-        :param target_spectra: The target spectra to back prop towards. Should be only 1 row
-        :param back_prop_epoch: #epochs to do the gradient descend
-        :param sess: The current session to do the back prop
-        """
-
-        #First initialize the starting points
-        RN = model_maker.initializeInBoundary(self.geometry_variable.shape, self.boundary)                #Get the random numbers
-        print("Random number within range", self.boundary)
-        assign_var_op = self.geometry_variable.assign(RN) #Assign the variable
-        sess.run([assign_var_op, train_init_op])
-
-        #Set up target output
-        target_spectra_repeat = np.repeat(target_spectra, self.batch_size, axis = 0)
-        #target_spectra_dataset = tf.data.Dataset.from_tensor_slices(target_spectra_repeat)
-        #target_spectra_dataset = target_spectra_dataset.repeat()
-        for i in range(back_prop_epoch):
-            loss_back_prop, optm_out, inferred_spectra = sess.run([self.loss, self.backprop_optm, self.logits], 
-                                                                feed_dict={self.labels: target_spectra_repeat})  
-            if (i % verb_step == 0):
-                print("Loss at inference step{} : {}".format(i,loss_back_prop))
-                if (loss_back_prop < stop_thres):
-                    print("Loss is lower than the threshold{}, inference stop".format(stop_thres)
-        #Then it is time to get the best performing one
-        Xpred, Ypred = sess.run([self.forward_in, self.logits])
-        loss_list = np.linalg.norm(Ypred - target_spectra_repeat, axis = 1)
-        best_estimate_index = np.argmin(loss_list)
-        print('Best error for point {} is having absolute error of {}'.formate(point_index, loss_list[best_estimate_index]))
-        Xpred_best = Xpred[best_estimate_index,:]
-        Ypred_best = Ypred[best_estimate_index,:]
-        return Xpred_best, Ypred_best
-        
 
 
     """

Backprop/evaluate.py
@@ -6,7 +6,7 @@ import Backprop_network_maker
 import network_helper
 import plotsAnalysis
 import os
-
+import flag_reader
 def compare_truth_pred(pred_file, truth_file):
     """
     Read truth and pred from csv files, compute their mean-absolute-error and the mean-squared-error
@@ -38,12 +38,50 @@ def evaluatemain(flags, eval_forward):
                                                                output_size=output_size-2*clip,
                                                                x_range=flags.x_range,
                                                                y_range=flags.y_range,
+                                                               geoboundary=flags.geoboundary,
                                                                cross_val=flags.cross_val,
                                                                val_fold=flags.val_fold,
                                                                batch_size=flags.batch_size,
                                                                shuffle_size=flags.shuffle_size,
                                                                normalize_input = flags.normalize_input,
 							        data_dir = flags.data_dir)
+    
+    #if the input is normalized
+    if flags.normalize_input:
+		    flags.boundary = [-1, 1, -1, 1]
+		
+		# make network
+    ntwk = Tandem_network_maker.TandemCnnNetwork(features, labels, model_maker.tandem_model, flags.batch_size,
+                                clip=flags.clip, forward_fc_filters=flags.forward_fc_filters,
+                                backward_fc_filters=flags.backward_fc_filters,reg_scale=flags.reg_scale,
+	                        learn_rate=flags.learn_rate,tconv_Fnums=flags.tconv_Fnums,
+				tconv_dims=flags.tconv_dims,n_branch=flags.n_branch,
+			        tconv_filters=flags.tconv_filters, n_filter=flags.n_filter,
+				decay_step=flags.decay_step, decay_rate=flags.decay_rate, boundary = flags.geoboundary,
+                                conv1d_filters = flags.conv1d_filters, conv_channel_list = flags.conv_channel_list)
+    # evaluate the results if the results do not exist or user force to re-run evaluation
+    save_file = os.path.join(os.path.abspath(''), 'data', 'test_pred_{}.csv'.format(flags.model_name))
+    if flags.force_run or (not os.path.exists(save_file)):
+        print('Evaluating the model ...')
+        pred_file, truth_file = ntwk.evaluate(valid_init_op, ckpt_dir=ckpt_dir,
+                                              model_name=flags.model_name, write_summary=True,
+                                              eval_forward = eval_forward)
+    else:
+        pred_file = save_file
+        truth_file = os.path.join(os.path.abspath(''), 'data', 'test_truth.csv')
+
+    mae, mse = compare_truth_pred(pred_file, truth_file)
+
+    plt.figure(figsize=(12, 6))
+    plt.hist(mse, bins=100)
+    plt.xlabel('Mean Squared Error')
+    plt.ylabel('cnt')
+    plt.suptitle('Tandem (Avg MSE={:.4e})'.format(np.mean(mse)))
+    plt.savefig(os.path.join(os.path.abspath(''), 'data',
+                             'tandem_{}.png'.format(flags.model_name)))
+    
+    
+    
     # make network
     ntwk = Backprop_network_maker.BackPropCnnNetwork(features, labels, model_maker.back_prop_model, flags.batch_size,
                             clip=flags.clip, forward_fc_filters=flags.forward_fc_filters,
@@ -55,7 +93,7 @@ def evaluatemain(flags, eval_forward):
 
     # evaluate the results if the results do not exist or user force to re-run evaluation
     save_file = os.path.join(os.path.abspath(''), 'data', 'test_pred_{}.csv'.format(flags.model_name))
-    if FORCE_RUN or (not os.path.exists(save_file)):
+    if flags.force_run or (not os.path.exists(save_file)):
         print('Evaluating the model ...')
         pred_file, truth_file = ntwk.evaluate(valid_init_op, train_init_op,
                                               ckpt_dir=ckpt_dir,back_prop_ephoch = 5000,
@@ -81,7 +119,7 @@ def evaluatemain(flags, eval_forward):
     print('Tandem (Avg MSE={:.4e})'.format(np.mean(mse)))
 
 if __name__ == '__main__':
-	flags = read_flag()
+	flags = flag_reader.read_flag()
 	evaluatemain(flags, eval_forward = False)
 	plotsAnalysis.SpectrumComparisonNGeometryComparison(3,2, (13,8), flags.model_name)	
 

Backprop/network_helper.py
@@ -37,6 +37,7 @@ class TrainValueHook(Hook):
         self.verb_step = verb_step
         self.loss = loss
         self.write_summary = write_summary
+        self.name = value_name
         if self.write_summary:
             assert ckpt_dir is not None
             self.train_mse_summary = HookValueSummary(value_name)
@@ -53,9 +54,30 @@ class TrainValueHook(Hook):
         if self.step % self.verb_step == 0:
             loss_val = sess.run(self.loss)
             if self.verb:
-                print('Step {}, loss: {:.2E}'.format(self.step, loss_val))
+                print('Step {},{}  loss: {:.2E}'.format(self.step, self.name, loss_val))
             if self.write_summary:
                 self.train_mse_summary.log(loss_val, self.step, sess, writer)
+
+class SummaryWritingHook(Hook):
+    """
+    This is a hook for writing summary periodically into the tensorboard
+    """
+    def __init__(self, summary_op, write_step = 3000):
+        """
+        :param write_step: The #steps to write to summary
+        :param summary_op: The summary operation to run
+        :param writer: write to 
+        """
+        super(SummaryWritingHook, self).__init__()
+        self.write_step = write_step
+        self.summary_op = summary_op
+    def run(self, sess, writer):
+        self.step +=1
+        if (self.step % self.write_step == 0):
+            summary = sess.run(self.summary_op)
+            writer.add_summary(summary, self.step)
+            writer.flush()
+            print("writing histograms")
 
 
 class ValidationHook(Hook):
@@ -63,7 +85,7 @@ class ValidationHook(Hook):
     This hook monitors performance on the validation set
     """
     def __init__(self, valid_step, valid_init_op, truth, pred, loss, stop_threshold, value_name = 'valid_mse', ckpt_dir=None, 
-                 write_summary=False, curve_num=6):
+                 write_summary=False, curve_num=6, low_validation_loss = 0.03):
         """
         Initialize the hook
         :param valid_step: # steps between evaluations
@@ -74,8 +96,8 @@ class ValidationHook(Hook):
         :param ckpt_dir: ckpt_dir: checkpoint directory, only use it if write_summary is True
         :param write_summary: log summary or not
         :param curve_num: #curve plots in validation images
-				:param stop_threshold: The Loss threshold to stop the algorithm
-				:param stop: Boolean value to stop the training
+	:param stop_threshold: The Loss threshold to stop the algorithm
+	:param stop: Boolean value to stop the training
         """
         super(ValidationHook, self).__init__()
         self.valid_step = valid_step
@@ -86,7 +108,9 @@ class ValidationHook(Hook):
         self.write_summary = write_summary
         self.curve_num = curve_num
         self.stop_threshold = stop_threshold
+        self.best_validation_loss = low_validation_loss #initialize the best validation to a low validation loss
         self.stop = False
+        self.save = False               #Whether save the session in this epoch
         if self.write_summary:
             assert ckpt_dir is not None
             self.valid_mse_summary = HookValueSummary(value_name)
@@ -103,6 +127,7 @@ class ValidationHook(Hook):
         :return:
         """
         self.step += 1
+        self.save = False
         if self.step % self.valid_step == 0 and self.step != 0:
             sess.run(self.valid_init_op)
             loss_val = []
@@ -116,12 +141,16 @@ class ValidationHook(Hook):
             loss_mean = np.mean(loss_val)
             print('Eval @ Step {}, loss: {:.2E}, duration {:.3f}s'.\
                   format(self.step, loss_mean, time.time()-self.time_cnt))
-            if loss_mean < self.stop_threshold:
-                print('Validation loss is lower than threshold{}, training is stopped'.format(self.stop_threshold))
-                self.stop = True
-            if math.isnan(loss_mean):
+            if math.isnan(loss_mean):   #If the loss is NAN, then Stop
                 print("The validation loss is NAN, please adjust (Lower) your learning rate and retrain. Aborting now")
                 self.stop  = True
+            else:                       #If the loss is not NAN
+                if loss_mean < self.stop_threshold:
+                    print('Validation loss is lower than threshold{}, training is stopped'.format(self.stop_threshold))
+                    self.stop = True
+                if loss_mean < self.best_validation_loss:       #If the loss is smaller than the best, then save this one now
+                    self.best_validation_loss = loss_mean
+                    self.save = True
             self.time_cnt = time.time()
             if self.write_summary:
                 self.valid_mse_summary.log(loss_mean, self.step, sess, writer)