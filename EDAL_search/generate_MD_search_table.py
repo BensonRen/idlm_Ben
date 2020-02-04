f0c89cccf436d2ce1dfded4d3df40d20fae1ccea
Ben <rensimiao.ben@gmail.com>
2020-02-04 10:10:57 -0500

    README update on paper search

---
+++ EDAL_search/generate_MD_search_table.py
@@ -0,0 +1,35 @@
+# This program is to generate a search markdown table for helena to scrape
+
+# Define the terms to search
+Energy_term_list = ["Wind", "Solar", "Power", "system", "Energy", "Generator", "Coal",\
+                    "Oil", "Natural Gas", "Geothermal", "Hydropower"]
+
+ML_term_list = ["machine learning", "deep learning", "support vector machine", \
+                "random forest", "regression tree", "neural network"]
+
+RS_term_list = ["remote sensing", "satellite", "aerial", "UAV",\
+                "unmanned aerial vehicle", "hyperspectral"]
+
+file_name = 'table.txt'
+
+if __name__ == '__main__':
+    with open(file_name, 'w') as f:
+        for i1, energy_term in enumerate(Energy_term_list):
+            for i2, ml_term in enumerate(ML_term_list):
+                for i3, rs_term in enumerate(RS_term_list):
+                    index_str =  str(i1 + 1) + "-" + str(i2 + 1) + "-" + str(i3 + 1)
+                    link = "https://scholar.google.com/scholar?hl=zh-CN&as_sdt=0%2C34&q=%22"
+                    link += energy_term
+                    link += "%22%3B+%22"
+                    link += ml_term
+                    link += "%22%3B+%22"
+                    link += rs_term
+                    link += "%22&btnG="
+                    f.write("|" + index_str + " | " + energy_term + " | " + ml_term + " | " \
+                            + rs_term + " | " + "[link]" + link + " |\n")
+
+
+
+
+
+
--- Pytorch/plotsAnalysis.py
+++ Pytorch/plotsAnalysis.py
@@ -13,6 +13,53 @@ from scipy.spatial import distance_matrix
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree
 
+
+def InferenceAccuracyExamplePlot(model_name, save_name, title, sample_num=10,  fig_size=(15,5), random_seed=1,
+                                 target_region=[0,300 ]):
+    """
+    The function to plot the Inference accuracy and compare with FFDS algorithm.
+    It takes the Ypred and Ytruth file as input and plot the first <sample_num> of spectras.
+    It also takes a random of 10 points to at as the target points.
+    :param model_name: The model name as the postfix for the Ytruth file
+    :param save_name:  The saving name of the figure
+    :param title:  The saving title of the figure
+    :param sample_num: The number of sample to plot for comparison
+    :param fig_size: The size of the figure
+    :param random_seed:  The random seed value
+    :param target_region:  The region that the targets get
+    :return:
+    """
+    # Get the prediction and truth file first
+    Ytruth_file = os.path.join('data','test_Ytruth_{}.csv'.format(model_name))
+    Ypred_file = os.path.join('data','test_Ypred_{}.csv'.format(model_name))
+    Ytruth = pd.read_csv(Ytruth_file, header=None, delimiter=' ').values
+    Ypred = pd.read_csv(Ypred_file, header=None, delimiter=' ').values
+
+    # Draw uniform random distribution for the reference points
+    np.random.seed(random_seed)     # To make sure each time we have same target points
+    targets = target_region[0] + (target_region[1] - target_region[0]) * np.random.uniform(low=0, high=1, size=10) # Cap the random numbers within 0-299
+    targets = targets.astype("int")
+    # Make the frequency into real frequency in THz
+    fre_low = 0.86
+    fre_high = 1.5
+    frequency = fre_low + (fre_high - fre_low)/len(Ytruth[0, :]) * np.arange(300)
+
+    for i in range(sample_num):
+        # Start the plotting
+        f = plt.figure(figsize=fig_size)
+        plt.title(title)
+        plt.scatter(frequency[targets], Ytruth[i,targets], label='S*')
+        plt.plot(frequency, Ytruth[i,:], label='FFDS')
+        plt.plot(frequency, Ypred[i,:], label='Candidate')
+        plt.legend()
+        plt.ylim([0,1])
+        plt.xlim([fre_low, fre_high])
+        plt.grid()
+        plt.xlabel("Frequency (THz)")
+        plt.ylabel("Transmittance")
+        plt.savefig(os.path.join('data',save_name + str(i) + '.png'))
+
+
 def RetrieveFeaturePredictionNMse(model_name):
     """
     Retrieve the Feature and Prediciton values and place in a np array
